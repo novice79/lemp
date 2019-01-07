d7ee003fc2c730d48b3e12653ec972224d5e0be9
novice <david@cninone.com>
2019-01-07 15:08:39 +0800

    if exist index.php just leave it alone

--- init.js
+++ init.js
@@ -123,35 +123,39 @@ fs.writeFileSync(sql_init_file, init_sql);
         }
     });
 })();
-let nginx_v = spawnSync('nginx', ['-v']);
-nginx_v = nginx_v.stdout.toString().trim() || nginx_v.stderr.toString().trim();
-const php_v = execSync('php -v').toString().trim();
-const mysql_v = execSync('mysql -V').toString().trim();
-const index =
-`
-<!DOCTYPE html> 
-<html> 
-<head> 
-<meta charset="utf-8" /> 
-<title>LEMP in docker test</title> 
-<style> 
-	body{ text-align:center} 
-	.version{ margin:0 auto; border:1px solid #F00} 
-</style> 
-</head> 
-<body> 
-    lemp versions:
-    <div class="version">    
-    ${nginx_v}<br>
-    ${php_v}<br>
-    ${mysql_v}
-	</div> 
-	<br>
-	<?php echo phpinfo(); ?>
-</body> 
-</html> 
-`;
-fs.writeFileSync('/var/www/index.php', index);
+const index_path = '/var/www/index.php';
+if ( !fs.existsSync(index_path) ) {
+    let nginx_v = spawnSync('nginx', ['-v']);
+    nginx_v = nginx_v.stdout.toString().trim() || nginx_v.stderr.toString().trim();
+    const php_v = execSync('php -v').toString().trim();
+    const mysql_v = execSync('mysql -V').toString().trim();
+    const index =
+    `
+    <!DOCTYPE html> 
+    <html> 
+    <head> 
+    <meta charset="utf-8" /> 
+    <title>LEMP in docker test</title> 
+    <style> 
+        body{ text-align:center} 
+        .version{ margin:0 auto; border:1px solid #F00} 
+    </style> 
+    </head> 
+    <body> 
+        lemp versions:
+        <div class="version">    
+        ${nginx_v}<br>
+        ${php_v}<br>
+        ${mysql_v}
+        </div> 
+        <br>
+        <?php echo phpinfo(); ?>
+    </body> 
+    </html> 
+    `;
+    fs.writeFileSync(index_path, index);
+}
+
 (function start_nginx() {
     const start_dt = new Date().getTime();
     const nginx = exec( `nginx` );
