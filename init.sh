b9f5ac4cad284585cf88a46b664405f296f6e1e4
novice <david@cninone.com>
2019-02-12 18:55:06 +0800

    fix bash script error

--- init.sh
+++ init.sh
@@ -29,6 +29,7 @@ then
 </html> 
 EOT
 elif [ -f "/var/www/public/index.php" ]
+then
     sed 's@/var/www@/var/www/public@g' -i /etc/nginx/conf.d/default.conf
 else
     echo "normal php www dir containing files, skip"
