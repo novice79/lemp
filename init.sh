909574176c90d0a6237d5fb866369bbc34d22778
novice <david@cninone.com>
2019-02-23 19:47:42 +0800

    use mysqld start service directly

--- init.sh
+++ init.sh
@@ -17,17 +17,19 @@ echo "MYSQL_USER=${MYSQL_USER:=david}"
 echo "MYSQL_PASSWORD=${MYSQL_PASSWORD:=freego}"
 echo "MYSQL_DATABASE=${MYSQL_DATABASE:=lemp}"
 cat <<EOT > $sql_init_file
--- DELETE FROM mysql.user ;
+DELETE FROM mysql.user ;
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL ON *.* TO '$MYSQL_USER'@'%';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL ON *.* TO '$MYSQL_USER'@'localhost';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 FLUSH PRIVILEGES;
 EOT
 
-mysqld_safe --init-file="${sql_init_file}" &
+mysqld --init-file="${sql_init_file}" --user=root &
 /usr/local/lsws/bin/lshttpd
 
 # no pgrep && ps
@@ -35,16 +37,16 @@ while [ 1 ]
 do
     sleep 2
     SERVICE="lshttpd"
-    if ! pidof "$SERVICE" >/dev/null
+    if [ ! pidof "$SERVICE" >/dev/null ]
     then
         echo "$SERVICE stopped. restart it"
         /usr/local/lsws/bin/lshttpd
     fi
 
     SERVICE="mysqld"
-    if ! pidof "$SERVICE" >/dev/null
+    if [ ! pidof "$SERVICE" >/dev/null ]
     then
         echo "$SERVICE stopped. restart it"
-        mysqld_safe --init-file="${sql_init_file}" &
+        mysqld --init-file="${sql_init_file}" --user=root &
     fi
 done
