e346d6edb91d5d7d9b826bc7da09782207cccb47
novice <david@cninone.com>
2019-02-26 20:35:25 +0800

    replace nginx to openlitespeed

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,11 @@ LABEL maintainer="David <david@cninone.com>"
 
 COPY ins_pack.sh /ins_pack.sh
 RUN /ins_pack.sh
-# need first install nginx, and then copy conf files
-COPY conf/nginx/default.conf /etc/nginx/conf.d/default.conf
-COPY conf/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # can not modify /etc/hosts here 
 # VOLUME ["/var/www", "/var/lib/mysql"]
 WORKDIR /var/www
-EXPOSE 80 3306 33060
+EXPOSE 80 443 3306 33060
 
 COPY init.sh /
 
--- ins_pack.sh
+++ ins_pack.sh
@@ -5,38 +5,30 @@ apt-get update && apt-get install -y tzdata curl wget procps net-tools \
 TZ="Asia/Chongqing"
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone 
 
-# nginx begin
-printf '%s\n' "deb http://nginx.org/packages/mainline/debian/ stretch nginx" >> /etc/apt/sources.list
-curl http://nginx.org/keys/nginx_signing.key | apt-key add -
-# nginx end
+echo "deb http://rpms.litespeedtech.com/debian/ stretch main" > /etc/apt/sources.list.d/lst_debian_repo.list
+wget -O /etc/apt/trusted.gpg.d/lst_debian_repo.gpg http://rpms.litespeedtech.com/debian/lst_debian_repo.gpg
+wget -O /etc/apt/trusted.gpg.d/lst_repo.gpg http://rpms.litespeedtech.com/debian/lst_repo.gpg
 
 # COPY --from=php:fpm /usr/local /usr/local
-# php begin
-wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add - \
-	&& echo "deb https://packages.sury.org/php/ stretch main" | tee /etc/apt/sources.list.d/php.list
-# php end
-apt-get update && apt-get install -y nginx nfs-common unzip \
-	php7.3 php7.3-bcmath php7.3-bz2 php7.3-cli php7.3-common php7.3-curl php7.3-dba php7.3-dev php7.3-enchant \
-	php7.3-fpm php7.3-gd php7.3-gmp php7.3-imap php7.3-intl php7.3-json php7.3-mbstring php7.3-mysql php7.3-odbc \
-	php7.3-opcache php7.3-pgsql php7.3-readline \
-	php7.3-soap php7.3-sqlite3 php7.3-tidy php7.3-xml php7.3-xmlrpc php7.3-zip php-redis php-igbinary php-mongodb \
+
+apt-get update && apt-get install -y nfs-common unzip \
+	openlitespeed lsphp73* \
 	&& apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false \
 	; rm -rf /var/lib/apt/lists/* \
 	; curl -o /usr/local/bin/composer https://getcomposer.org/download/1.8.4/composer.phar \
 	&& chmod +x /usr/local/bin/composer
 
 cd / 
-# wget https://wordpress.org/latest.tar.gz && tar zxf latest.tar.gz 
-# mkdir -p /wordpress/wp-content/languages
-# wget https://downloads.wordpress.org/translation/core/5.0.3/zh_CN.zip && unzip zh_CN.zip -d /wordpress/wp-content/languages
-wget https://cn.wordpress.org/wordpress-5.0.3-zh_CN.tar.gz && tar zxf *.tar.gz
+wget https://wordpress.org/latest.tar.gz && tar zxf latest.tar.gz 
+mkdir -p /wordpress/wp-content/languages
+wget https://downloads.wordpress.org/translation/core/5.0.3/zh_CN.zip && unzip zh_CN.zip -d /wordpress/wp-content/languages
+# wget https://cn.wordpress.org/wordpress-5.0.3-zh_CN.tar.gz && tar zxf *.tar.gz
 rm -f *.{tar.gz,zip}
 mv /wordpress/wp-config-sample.php /wordpress/wp-config.php
 sed "/DB_COLLATE/a define('WPLANG', 'zh_CN');" -i /wordpress/wp-config.php
 
-mkdir /var/www ; chown -R www-data:www-data /var/www ; ln -sf /usr/sbin/php-fpm7.3 /usr/sbin/php-fpm ; \
-	sed 's@^listen = /run.*$@listen = 127.0.0.1:9000@g' -i /etc/php/7.3/fpm/pool.d/www.conf ; \
-	sed '/\[mysqld\]/a default_authentication_plugin=mysql_native_password' -i /etc/mysql/conf.d/docker.cnf
+mkdir /var/www ; chown -R www-data:www-data /var/www ;
+sed '/\[mysqld\]/a default_authentication_plugin=mysql_native_password' -i /etc/mysql/conf.d/docker.cnf
 
 
 rm -- "$0"
